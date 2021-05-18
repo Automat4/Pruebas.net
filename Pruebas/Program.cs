588aa971e25534cca0f6074fe0031c745e1294c1
Autómat4 <av4loncr@hotmail.com>
2021-05-18 16:32:46 -0600

    Agregar archivos de proyecto.

---
+++ Pruebas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var result = Prueba(8);
+            Console.WriteLine(result);
+        }
+
+        public static int Prueba(int num)
+        {
+            if (num > 1)
+                return 2;
+            else return 0;
+        }
+    }
+
+}
